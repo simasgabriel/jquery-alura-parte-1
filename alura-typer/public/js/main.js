dc11e9e77b77572ca8f2d8cb8d73fcf07cb7fa6f
simasgabriel <45041309+simasgabriel@users.noreply.github.com>
2023-02-02 16:34:22 -0300

    inicio construcao placar

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -7,8 +7,6 @@ tamanhoFrase.text(numeroPalavras);
 let campo = $(".campoDigitacao");
 let botao = $(".botao");
 
-
-
 campo.on("input", function () {
     let conteudoCampo = campo.val();
     let quantidadePalavras = conteudoCampo.split(/\S+/).length;
@@ -19,14 +17,46 @@ campo.on("input", function () {
 
 })
 
+
+
+
+
+
+
 let tempoRestante = $("#tempo").text();
 campo.on("focus", function () {
-    setInterval(function () {
+    let cronometroID = setInterval(function () {
         tempoRestante--;
         $("#tempo").text(tempoRestante);
-        if (tempoRestante <= 1) {
-            campo.attr("disabled", true)
-            tempoRestante = 0;
+        if (tempoRestante < 1) {
+            campo.attr("disabled", true);
+            $("#tempo").text("Tempo encerrado!");
+            $("#secs").text(" ");
+            clearInterval(cronometroID);
         }
     }, 1000);
+
+
 })
+
+
+
+
+
+
+$("#reiniciarJogo").on("click", function () {
+
+    campo.attr("disabled", false);
+    tempoRestante = 3;
+    $("#tempo").text("3");
+
+
+
+    $("#secs ").text(" secs");
+
+    $("#contadorPalavras").text(0);
+    $("#contadorCaracteres").text(0);
+
+
+
+});
